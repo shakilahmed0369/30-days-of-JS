d3c9cc3bc3decbeefc6b9a2b7e0a8477741b49ab
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-08-13 22:22:33 +0600

    Day 3: Understand primitive data types

---
+++ day_3/script.js
@@ -0,0 +1,18 @@
+/**
+ * Primitive data type
+ * -------------------
+ * In javascript primitive data types are basic building blocks of data.
+ * 
+ * They are immutable (can not be changed)
+ * and are directly stored in memory. javascript jas the follings primitive datatypes
+ * 
+ * 1. String: Represents a sequence of characters. it is enclosed in single quotes ('') or double quotes ("") in javascript. Example: "Hello", 'world'
+ * 
+ * 2. Number: Represents numeric values, including integers and floating pints numbers. Example 369, 4.3
+ * 
+ * 3. Boolean: Represents a logical value of either true or false. Example: true, false
+ * 
+ * 4. Null: Represents the intentional absence of any object value. Example: null
+ * 
+ * 5. Undefined: Represents a a variable that has been declared but has no assigned value
+ */
