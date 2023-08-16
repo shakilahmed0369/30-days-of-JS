6ba1e67a1e29d30fc14916d948323abe620d32ca
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-08-17 00:53:11 +0600

    Day 5: Parameter, Argument and return statement

---
+++ day_5/script.js
@@ -0,0 +1,17 @@
+/** Parameter, Arguments, return statement */
+/**
+ * Parameter
+ * ---------
+ * in basics words parameters are the variables that are used to pass value into a function or method. they act as a placeholders for the actual value
+ * 
+ * Arguments:
+ * ----------
+ * Arguments are the actual values that are passed into a function or method when it is called. those value are assigned to the corresponding
+ * parameters define in the function or method declaration.
+ * 
+ * Return statement
+ * ----------------
+ * The return statement is used to specify the value that should the function
+ * return after it has finished execution.
+ * 
+ */
