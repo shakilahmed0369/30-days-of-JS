45706158a3d9d87155e286783fc7ab7db8927411
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-09-05 07:13:52 +0600

    Day 7: Array methods, push

---
+++ day_7/script.js
@@ -0,0 +1,43 @@
+/** javaScript Array Methods 
+ * --------------------------
+ * javaScript Array Methods Helps to Manipulate and iterate array
+ * javaScript Have Wide Variety of Array Methods
+ * 
+ * push(): Adds one or more elements to the end of an array.
+ * 
+ * pop(): Removes the last element from an array.
+ * 
+ * shift(): Removes the first element from an array.
+ * 
+ * unshift(): Adds one or more elements to the beginning of an array.
+ * 
+ * slice(): Extracts a section of an array and returns it as a new array.
+ * 
+ * indexOf(): Returns the index of the first occurrence of a value in an array.
+ * 
+ * lastIndexOf(): Returns the index of the last occurrence of a value in an array.
+ * 
+ * concat(): Combines two or more arrays into a new array.
+ * 
+ * filter(): Creates a new array with the elements that pass a test function.
+ * 
+ * map(): Creates a new array with the results of applying a function to each element of an array.
+ * 
+ * reduce(): Reduces an array to a single value by repeatedly applying a function to the elements.
+ * 
+ * More...
+*/
+
+const fruits = ['appel', 'banana', 'coconut', 'sugarcane'];
+
+// PUSH: push items in array
+fruits.push('orange');
+// PUSH: multiple items in array 
+fruits.push('guava', 'mango');
+
+// PUSH: push array
+fruits.push(['orange', 'dragon fruit']);
+
+console.log(fruits);
+
+
